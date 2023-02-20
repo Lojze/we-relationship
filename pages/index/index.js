37e15aa1152b737cbf68626f63b63e58e2fda96f
lojze.wang <lojze.wang@ly.com>
2023-02-20 18:26:38 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,26 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    tabs:[
+      {
+        name:"关系找称呼"
+      },
+      {
+        name:"称呼找关系"
+      },
+      {
+        name:"两者间称呼"
+      },
+      {
+        name:"两者合称"
+      },
+    ]
+  },
+  
+  onLoad() {
+    
+  }
+})
